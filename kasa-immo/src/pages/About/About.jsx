8cec3f73186be2fa992681945e7443d8b5be9528
Thomas PINEAU <thomas.pineau93@proton.me>
2025-03-03 14:34:51 +0100

    Remove unused cover images and update imports to use webp format in About and Home pages

--- kasa-immo/src/pages/About/About.jsx
+++ kasa-immo/src/pages/About/About.jsx
@@ -1,32 +1,27 @@
 import React from "react";
 
 import Banner from "../../components/Banner/Banner";
-import imageSrc from '../../assets/covers/Cover2.png'
+import imageSrc from "../../assets/covers/Cover2.webp";
 import Accordion from "../../components/Accordion/Accordion";
 
-import AboutData from '../../datas/about.json'
-import './_about.scss'
+import AboutData from "../../datas/about.json";
+import "./_about.scss";
 
 function About() {
-
-    return (
-        <div className="about">
-            <Banner
-                imageSrc={imageSrc}
-                text={""}
-            />
-            <div className="about__accordion-wrapper">
-                {AboutData.map((accordion) => (
-                    <Accordion
-                        key={accordion.id}
-                        title={accordion.title}
-                        content={accordion.content}
-                    />
-                ))}
-            </div>
-        </div>
-    )
-
+	return (
+		<div className="about">
+			<Banner imageSrc={imageSrc} text={""} />
+			<div className="about__accordion-wrapper">
+				{AboutData.map((accordion) => (
+					<Accordion
+						key={accordion.id}
+						title={accordion.title}
+						content={accordion.content}
+					/>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default About;
--- kasa-immo/src/pages/Home/Home.jsx
+++ kasa-immo/src/pages/Home/Home.jsx
@@ -5,31 +5,27 @@ import CardGallery from "../../components/CardGallery/CardGallery";
 import CardItem from "../../components/CardItem/CardItem";
 
 import LogementsData from "../../datas/logements.json";
-import imageSrc from '../../assets/covers/Cover1.png'
+import imageSrc from "../../assets/covers/Cover1.webp";
 
-import './_home.scss';
+import "./_home.scss";
 
 function Home() {
-
-    const bannerText = "Chez vous, partout et ailleurs"
-    return (
-        <div className="home">
-            <Banner
-                imageSrc={imageSrc}
-                text={bannerText}
-            />
-            <CardGallery>
-                {LogementsData.map((logement) => (
-                    <CardItem
-                        key={`${logement.id}-${logement.title}`}
-                        id={logement.id}
-                        title={logement.title}
-                        cover={logement.cover}
-                    />
-                ))}
-            </CardGallery>
-        </div>
-    )
+	const bannerText = "Chez vous, partout et ailleurs";
+	return (
+		<div className="home">
+			<Banner imageSrc={imageSrc} text={bannerText} />
+			<CardGallery>
+				{LogementsData.map((logement) => (
+					<CardItem
+						key={`${logement.id}-${logement.title}`}
+						id={logement.id}
+						title={logement.title}
+						cover={logement.cover}
+					/>
+				))}
+			</CardGallery>
+		</div>
+	);
 }
 
 export default Home;
